2b610c554e513a090cad95a5f1f4c5ed01b92744
alfyrje <alfyrje@gmail.com>
2024-10-24 16:24:21 +0700

    integrated postgreSQL

--- QAirline/settings.py
+++ QAirline/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'QAirline.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'qAirline',
+        'USER': 'qAirline',
+        'PASSWORD': 'denhothoi',
+        'HOST': '127.0.0.1'
     }
 }
 
