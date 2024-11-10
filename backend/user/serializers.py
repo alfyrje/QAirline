ea0fb286bf1ec2ec34949868e2aee7c7e038a291
Ha Tang <121566444+hachane@users.noreply.github.com>
2024-11-10 14:06:07 +0700

    viet lai user model

--- backend/user/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import User
-
-class UserSerializer(seralizers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['code', 'user_name', 'password', 'name', 'age', 'email', 'phone_number', 'address', 'created_at']
-        read_only_fields = ['user_name','created_at']
