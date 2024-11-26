0643147f983f7b15571014613a7dc0fe4a9e7192
Ha Tang <121566444+hachane@users.noreply.github.com>
2024-11-26 08:32:56 +0700

    fix

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -35,8 +35,8 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         personal_info_data = validated_data.pop('personal_info')
         passenger = Passenger.objects.create(**personal_info_data)
-        user.personal_info = passenger
         user = User.objects.create(**validated_data)
+        user.personal_info = passenger
         user.set_password(validated_data['password'])
         user.save()
         return user
