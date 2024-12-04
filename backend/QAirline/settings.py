4c021e01e92f7ab0b164e1cd903775b2bf8e09bf
Ha Tang <121566444+hachane@users.noreply.github.com>
2024-12-04 08:28:29 +0700

    fix ui flight-booking

--- backend/QAirline/settings.py
+++ backend/QAirline/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'django_dyn_api',
+    # 'django_dyn_api',
     'rest_framework',
     'rest_framework_simplejwt',
 ]
