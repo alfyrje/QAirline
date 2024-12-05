5d021c12d4e26003a9fb82229708cc5349b740a3
alfyrje <alfyrje@gmail.com>
2024-12-05 10:38:28 +0700

    added url

--- backend/flights/urls.py
+++ backend/flights/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import FlightSearchView, CreateTicketsAPI, TicketsFlightsHistoryAPI, BookedSeatsView
+from .views import FlightSearchView, CreateTicketsAPI, TicketsFlightsHistoryAPI, BookedSeatsView, FlightLocationsView
 
 urlpatterns = [
     path('search-flights/', FlightSearchView.as_view(), name='flight-search'),
     path('create-tickets/', CreateTicketsAPI.as_view(), name='book-ticket'),
     path('tickets-history/', TicketsFlightsHistoryAPI.as_view(), name='tickets-history'),
     path('booked-seats/', BookedSeatsView.as_view(), name='booked-seats'),
+    path('locations/', FlightLocationsView.as_view(), name='locations'),
 ]   
